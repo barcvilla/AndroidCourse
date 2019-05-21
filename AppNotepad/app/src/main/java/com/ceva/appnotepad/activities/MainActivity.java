2f173eedff10690c08a1674f4e44ff0155e1e36d
Carlos Villanueva <ceva_19@hotmail.com>
2019-05-21 15:32:20 -0300

    Java Android Programming

--- AppNotepad/app/src/main/java/com/ceva/appnotepad/activities/MainActivity.java
+++ AppNotepad/app/src/main/java/com/ceva/appnotepad/activities/MainActivity.java
@@ -1,7 +1,13 @@
 package com.ceva.appnotepad.activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,12 +16,52 @@ import com.ceva.appnotepad.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Toolbar toolbar;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView = (NavigationView)findViewById(R.id.navigationView);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.isChecked())
+                {
+                    menuItem.setChecked(false);
+                }
+                else
+                {
+                    menuItem.setChecked(true);
+                }
+                drawerLayout.closeDrawers();
+                return false;
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle =
+                new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open, R.string.close)
+                {
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        super.onDrawerOpened(drawerView);
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        super.onDrawerClosed(drawerView);
+                    }
+                };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 }
