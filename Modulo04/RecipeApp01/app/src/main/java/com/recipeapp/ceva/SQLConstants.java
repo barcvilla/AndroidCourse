811f21e10d6008777352b271473e74d5ff011489
Carlos Villanueva <ceva_19@hotmail.com>
2019-05-29 23:11:48 -0300

    Java Android Programming - Advanced

---
+++ Modulo04/RecipeApp01/app/src/main/java/com/recipeapp/ceva/SQLConstants.java
@@ -0,0 +1,38 @@
+package com.recipeapp.ceva;
+
+/**
+ * Definimos las variables utilizable en el trabajo con BD
+ */
+public class SQLConstants
+{
+    //DB
+    public static final String DB = "dbrecetas.db";
+
+    //Tables
+    public static final String TABLE_RECETAS = "recetas";
+
+    //Colums
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NOMBRE = "nombre";
+    public static final String COLUMN_PERSONAS = "personas";
+    public static final String COLUMN_DESCRIPCION = "descripcion";
+    public static final String COLUMN_PREPARACION = "preparacion";
+    public static final String COLUMN_IMAGE = "image";
+    public static final String COLUMN_FAV = "fav";
+
+    //Queries
+    public static final String SQL_CREATE_TABLE_RECETAS =
+            "CREATE TABLE " + TABLE_RECETAS +
+            "(" +
+                COLUMN_ID + " TEXT PRIMARY KEY," +
+                COLUMN_NOMBRE + " TEXT," +
+                COLUMN_PERSONAS + " INT," +
+                COLUMN_DESCRIPCION + " TEXT," +
+                COLUMN_PREPARACION + " TEXT," +
+                COLUMN_IMAGE + " TEXT," +
+                COLUMN_FAV + " INT" +
+            ");";
+
+    public static final String SQL_DROP_TABLE = "DROP TABLE " + TABLE_RECETAS;
+
+}
---
+++ Modulo04/RecipeApp01/app/src/main/java/com/recipeapp/ceva/helpers/DBHelper.java
@@ -0,0 +1,31 @@
+package com.recipeapp.ceva.helpers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.recipeapp.ceva.SQLConstants;
+
+/**
+ * Clase que nos ayuda a trabajar con las conexiones
+ */
+public class DBHelper extends SQLiteOpenHelper
+{
+    public static final int DB_VERSION = 1;
+
+    public DBHelper(Context context)
+    {
+        super(context, SQLConstants.DB, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQLConstants.SQL_CREATE_TABLE_RECETAS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQLConstants.SQL_DROP_TABLE);
+        onCreate(db);
+    }
+}
